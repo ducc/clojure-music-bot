39c965019b3cf252cdf25e3bd321eda7cc9d1036
SpongyBacon <spongybacon@hotmail.co.uk>
2017-11-15 06:50:22 +0000

    more functions!!!!!

--- src/musicbot/core.clj
+++ src/musicbot/core.clj
@@ -15,21 +15,23 @@
 (defn sendMessage [event, message] 
     (-> event .getTextChannel (.sendMessage message) .queue))
 
-(defn handlePingCommand [event]
+(defn handlePingCmd [event]
     (sendMessage event "Pong!"))
 
-(defn handleHelpCommand [event]
+(defn handleHelpCmd [event]
     (sendMessage event "Get some help..."))
 
+(defn isCmdName [comparison, cmd]
+    (.equalsIgnoreCase cmd comparison))
+
 (defn handlePrefixMatch [event, message]
-    (def args (.split (.substring message (.length prefix)) "\\s+"))
-    (def name (nth args 0))
+    (def args (.split (.substring message (count prefix)) "\\s+"))
+    (def cmdName (nth args 0))
     (cond 
-        (.equalsIgnoreCase name "ping") 
-            (handlePingCommand event)
-        (.equalsIgnoreCase name "help") 
-            (handleHelpCommand event)
-    ))
+        (isCmdName cmdName "ping") 
+            (handlePingCmd event)
+        (isCmdName cmdName "help") 
+            (handleHelpCmd event)))
 
 (def listener 
     (proxy [ListenerAdapter] []
@@ -53,4 +55,4 @@
         (.buildAsync)))
 
     ; ya we made it
-    (prn "JDA now running :)"))
+    (prn "JDA initialized!"))
