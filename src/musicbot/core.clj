bb20815d7fa2c1e7789da3a7cd5563c25c19d9b7
SpongyBacon <spongybacon@hotmail.co.uk>
2017-11-15 05:56:08 +0000

    very basic command handling

--- src/musicbot/core.clj
+++ src/musicbot/core.clj
@@ -4,30 +4,53 @@
         [net.dv8tion.jda.core JDABuilder AccountType JDA]
         [net.dv8tion.jda.core.hooks ListenerAdapter]))
 
+(def prefix "RONNIEPICKERING")
+
+(defn messageContent [event]
+    (-> event .getMessage .getContent))
+
 (defn compareMsgContent [event, check]
-    (.equals (-> event .getMessage .getContent) check))
+    (.equals (messageContent event) check))
 
 (defn sendMessage [event, message] 
     (-> event .getTextChannel (.sendMessage message) .queue))
 
-(defn listener []
+(defn handlePingCommand [event]
+    (sendMessage event "Pong!"))
+
+(defn handleHelpCommand [event]
+    (sendMessage event "Get some help..."))
+
+(defn handlePrefixMatch [event, message]
+    (def args (.split (.substring message (.length prefix)) "\\s+"))
+    (def name (nth args 0))
+    (cond 
+        (.equalsIgnoreCase name "ping") 
+            (handlePingCommand event)
+        (.equalsIgnoreCase name "help") 
+            (handleHelpCommand event)
+    ))
+
+(def listener 
     (proxy [ListenerAdapter] []
         (onMessageReceived [event] 
-            (if (compareMsgContent event "RONNIEPICKERING")
-                (sendMessage event "DO YOU KNOW WHO I AM?")))))
+            (def message (messageContent event))
+            (if (.startsWith message prefix)
+                (handlePrefixMatch event message)))
+        (onStatusChange [event]
+            (def status (-> event .getStatus .name))
+            (prn (str "Status changed to " status)))))
 
-(defn -main
-    [& args]
-    
+(defn -main [& args]
     ; require token as argument
     (if (= (count args) 0)
         (throw (RuntimeException. "lol no token bro")))
 
     ; build jda, add listener & run async!
-    (-> (JDABuilder. (AccountType/BOT)) 
+    (def jda (-> (JDABuilder. (AccountType/BOT)) 
         (.setToken (first args))
-        (.addEventListener (into-array [(listener)]))
-        (.buildAsync))
+        (.addEventListener (into-array [listener]))
+        (.buildAsync)))
 
     ; ya we made it
     (prn "JDA now running :)"))
